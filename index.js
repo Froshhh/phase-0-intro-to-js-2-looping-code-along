a4d05f9566cae738ce0264bd07b450c7831f1113
Frank Trevino <fjoshuat@gmail.com>
2022-11-08 16:28:15 -0600

    commit tree f4e8c4a66de0de240ff313aaa3941112fb23f250

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(to, event) {
+    let messages = []
+    for (let i = 0; i < to.length; i++) {
+        messages.push((`Thank you, ${to[i]}, for the wonderful ${event} gift!`))
+    }
+    return messages;
+}
+
+function countDown(i) {
+    while (i > -1) {
+        console.log (i);
+        i--;
+    }
+    return i; 
+}
